8d0315c99736894815780050ff2a462de5d4d006
viganod <viganod@host.docker.internal>
2024-12-10 11:42:16 +0100

    First Commit

---
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/ApplicazioneBancariaApplication.java
@@ -0,0 +1,13 @@
+package eu.tasgroup.applicativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicazioneBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicazioneBancariaApplication.class, args);
+	}
+
+}
---
+++ applicazione-bancaria/src/test/java/eu/tasgroup/applicativo/ApplicazioneBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.tasgroup.applicativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicazioneBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
