c36b8aa997dc88fe9f9bd81a70e0a358e7b41921
samuel-mastrelli-tas <samuel.mastrelli@tasgroup.eu>
2024-12-10 20:06:53 +0100

    Added: Presito, Carta

---
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/businesscomponent/model/mysql/Carta.java
@@ -0,0 +1,109 @@
+package eu.tasgroup.applicativo.businesscomponent.model.mysql;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "carte_di_credito")
+public class Carta implements Serializable {
+
+
+	private static final long serialVersionUID = -7060237652529379132L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "cod_carta")
+	private long codCarta;
+	
+	@Column(name = "numero_carta", nullable = false)
+	private String numeroCarta;
+
+	@Column(name = "data_scadenza", nullable = false)
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+	private Date dataScadenza;
+	
+	@Column(name = "cvv", nullable = false)
+	private String cvv;
+	
+	@ManyToOne
+	@JoinColumn(name = "cod_cliente", nullable = false)
+	private Cliente cliente;
+
+	public long getCodCarta() {
+		return codCarta;
+	}
+
+	public void setCodCarta(long codCarta) {
+		this.codCarta = codCarta;
+	}
+
+	public String getNumeroCarta() {
+		return numeroCarta;
+	}
+
+	public void setNumeroCarta(String numeroCarta) {
+		this.numeroCarta = numeroCarta;
+	}
+
+	public Date getDataScadenza() {
+		return dataScadenza;
+	}
+
+	public void setDataScadenza(Date dataScadenza) {
+		this.dataScadenza = dataScadenza;
+	}
+
+	public String getCvv() {
+		return cvv;
+	}
+
+	public void setCvv(String cvv) {
+		this.cvv = cvv;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cliente, codCarta, cvv, dataScadenza, numeroCarta);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Carta other = (Carta) obj;
+		return Objects.equals(cliente, other.cliente) && codCarta == other.codCarta && Objects.equals(cvv, other.cvv)
+				&& Objects.equals(dataScadenza, other.dataScadenza) && Objects.equals(numeroCarta, other.numeroCarta);
+	}
+
+	@Override
+	public String toString() {
+		return "Carta [codCarta=" + codCarta + ", numeroCarta=" + numeroCarta + ", dataScadenza=" + dataScadenza
+				+ ", cvv=" + cvv + ", cliente=" + cliente + "]";
+	}
+
+	
+}
--- applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/businesscomponent/model/mysql/Prestito.java
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/businesscomponent/model/mysql/Prestito.java
@@ -1,5 +1,107 @@
 package eu.tasgroup.applicativo.businesscomponent.model.mysql;
 
-public class Prestito {
+import java.io.Serializable;
+import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "prestiti")
+public class Prestito implements Serializable {
+
+
+	private static final long serialVersionUID = 8837782913642560317L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "cod_prestito")
+	private long codPrestito; 
+
+	@Column(name="importo", nullable = false)
+	private double importo;
+	
+	@Column(name = "tasso_interesse", nullable = false)
+	private double tassoInteresse;
+	
+	@Column(name = "durata_mesi", nullable = false)
+	private int durataMesi;
+	
+	@ManyToOne
+	@JoinColumn(name="cod_cliente", nullable = false)
+	private Cliente cliente;
+
+	public long getCodPrestito() {
+		return codPrestito;
+	}
+
+	public void setCodPrestito(long codPrestito) {
+		this.codPrestito = codPrestito;
+	}
+
+	public double getImporto() {
+		return importo;
+	}
+
+	public void setImporto(double importo) {
+		this.importo = importo;
+	}
+
+	public double getTassoInteresse() {
+		return tassoInteresse;
+	}
+
+	public void setTassoInteresse(double tassoInteresse) {
+		this.tassoInteresse = tassoInteresse;
+	}
+
+	public int getDurataMesi() {
+		return durataMesi;
+	}
+
+	public void setDurataMesi(int durataMesi) {
+		this.durataMesi = durataMesi;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cliente, codPrestito, durataMesi, importo, tassoInteresse);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Prestito other = (Prestito) obj;
+		return Objects.equals(cliente, other.cliente) && codPrestito == other.codPrestito
+				&& durataMesi == other.durataMesi
+				&& Double.doubleToLongBits(importo) == Double.doubleToLongBits(other.importo)
+				&& Double.doubleToLongBits(tassoInteresse) == Double.doubleToLongBits(other.tassoInteresse);
+	}
+
+	@Override
+	public String toString() {
+		return "Prestito [codPrestito=" + codPrestito + ", importo=" + importo + ", tassoInteresse=" + tassoInteresse
+				+ ", durataMesi=" + durataMesi + ", cliente=" + cliente + "]";
+	}
+	
+	
 }
