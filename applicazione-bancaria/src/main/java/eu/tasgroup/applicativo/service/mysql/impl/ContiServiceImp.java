aab067749bf8dbd05d6639583ff1229901a85453
davide-vigano-tas <davide.vigano@tasgroup.eu>
2024-12-11 12:18:47 +0100

    Add Conti service impl modify conti service

---
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/service/mysql/impl/ContiServiceImp.java
@@ -0,0 +1,49 @@
+package eu.tasgroup.applicativo.service.mysql.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import eu.tasgroup.applicativo.businesscomponent.model.mysql.Conto;
+import eu.tasgroup.applicativo.repository.mysql.ContiRepository;
+import eu.tasgroup.applicativo.service.mysql.ContiService;
+
+@Service
+public class ContiServiceImp implements ContiService {
+	@Autowired
+	ContiRepository contiRepository;
+
+	@Override
+	public double saldoMedio(Conto conto) {
+		return contiRepository.saldoMedio();
+	}
+
+	@Override
+	public void deleteContoById(long id) {
+		contiRepository.deleteById(id);
+	}
+
+	@Override
+	public List<Conto> getContiSaldoZero() {
+		return contiRepository.getContiSaldoZero();
+	}
+
+	@Override
+	public Conto createOrUpdate(Conto conto) {
+		return contiRepository.saveAndFlush(conto);
+	}
+
+	@Override
+	public Optional<Conto> findById(long id) {
+		return contiRepository.findById(id);
+	}
+
+	@Override
+	public List<Conto> getAll() {
+		return contiRepository.findAll();
+	}
+	
+	
+}
