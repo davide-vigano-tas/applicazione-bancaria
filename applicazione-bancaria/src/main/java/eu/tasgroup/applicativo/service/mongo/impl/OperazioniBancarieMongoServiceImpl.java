b73fc58b4b16541858a1320916cb0de53ad91ae1
samuel-mastrelli-tas <samuel.mastrelli@tasgroup.eu>
2024-12-11 14:03:08 +0100

    OperazioniBancarieServiceImpl

---
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/service/mongo/impl/OperazioniBancarieMongoServiceImpl.java
@@ -0,0 +1,53 @@
+package eu.tasgroup.applicativo.service.mongo.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import eu.tasgroup.applicativo.businesscomponent.model.mongo.OperazioniBancarieMongo;
+import eu.tasgroup.applicativo.repository.mongo.OperazioniBancarieMongoRepository;
+import eu.tasgroup.applicativo.service.mongo.OperazioniBancarieMongoService;
+
+public class OperazioniBancarieMongoServiceImpl implements OperazioniBancarieMongoService {
+	
+	private OperazioniBancarieMongoRepository operazioniBancarieMongoRepository;
+	
+	public OperazioniBancarieMongoServiceImpl(OperazioniBancarieMongoRepository operazioniBancarieMongoRepository) {
+		this.operazioniBancarieMongoRepository = operazioniBancarieMongoRepository;
+	}
+
+	@Override
+	public Optional<OperazioniBancarieMongo> findById(String id) {
+		return operazioniBancarieMongoRepository.findById(id);
+	}
+
+	@Override
+	public OperazioniBancarieMongo createOrUpdate(OperazioniBancarieMongo operazione) {
+		return operazioniBancarieMongoRepository.save(operazione);
+	}
+
+	@Override
+	public List<OperazioniBancarieMongo> findAll() {
+		return operazioniBancarieMongoRepository.findAll();
+	}
+
+	@Override
+	public double totaleImporto() {
+		return operazioniBancarieMongoRepository.importoTotale();
+	}
+
+	@Override
+	public List<OperazioniBancarieMongo> operazioniPerTipo(String tipo) {
+		return operazioniBancarieMongoRepository.operazioniPerTipo(tipo);
+	}
+
+	@Override
+	public List<OperazioniBancarieMongo> ultimeOperazioni() {
+		return operazioniBancarieMongoRepository.ultimeOperazioni();
+	}
+
+	@Override
+	public double importoMedioOperazione() {
+		return operazioniBancarieMongoRepository.importoMedioOperazioni();
+	}
+
+}
