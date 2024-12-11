9a81719037a2227f951dc7f2d6f7b1c2fca143e7
samuel-mastrelli-tas <samuel.mastrelli@tasgroup.eu>
2024-12-11 15:54:00 +0100

    Fixed Conto, pom.xml

--- applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/businesscomponent/model/mysql/Conto.java
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/businesscomponent/model/mysql/Conto.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 
 import eu.tasgroup.applicativo.businesscomponent.enumerated.TipoConto;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -12,7 +13,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "conti")
 public class Conto implements Serializable {
 
 	private static final long serialVersionUID = -5781053561417393743L;
