793a144bf128ef7eaae0ebaa048b1dde490d2630
davide-vigano-tas <davide.vigano@tasgroup.eu>
2024-12-11 12:40:13 +0100

    add cartaService e impl

---
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/service/mysql/CartaService.java
@@ -0,0 +1,17 @@
+package eu.tasgroup.applicativo.service.mysql;
+
+import java.util.List;
+import java.util.Optional;
+
+import eu.tasgroup.applicativo.businesscomponent.model.mysql.Carta;
+
+public interface CartaService  {
+	
+	Carta createOrUpdate(Carta carta);
+	
+	void deleteCartaById(long id);
+	
+	Optional<Carta> getCartaById(long id);
+	
+	List<Carta> getAll();
+}
