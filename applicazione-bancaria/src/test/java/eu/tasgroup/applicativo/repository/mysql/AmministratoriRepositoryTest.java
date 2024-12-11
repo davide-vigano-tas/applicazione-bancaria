678af8b48edd44fe57c783b1dfd1d3cd138fb894
davide-vigano-tas <davide.vigano@tasgroup.eu>
2024-12-11 16:31:48 +0100

    Add test Admin

--- applicazione-bancaria/src/test/java/eu/tasgroup/applicativo/repository/mysql/AmministratoriRepositoryTest.java
+++ applicazione-bancaria/src/test/java/eu/tasgroup/applicativo/repository/mysql/AmministratoriRepositoryTest.java
@@ -1,38 +1,39 @@
 package eu.tasgroup.applicativo.repository.mysql;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import eu.tasgroup.applicativo.businesscomponent.model.mysql.Amministratore;
 import eu.tasgroup.applicativo.repository.AmministratoriRepository;
 
 @SpringBootTest
-@DataJpaTest
 class AmministratoriRepositoryTest {
-	@Autowired 
-	private AmministratoriRepository ar;
-	@Test
-	void testFindByEmailAdmin() {
-		Amministratore admin = new Amministratore();
-		
-		admin.setNomeAdmin("Prova");
-		admin.setCognomeAdmin("Prova");
-		admin.setEmailAdmin("mail@mail.it");
-		admin.setPasswordAdmin("pass");
-		
-		ar.save(admin);
-		
-		Optional<Amministratore> adminSalvato = ar.findByEmailAdmin(admin.getEmailAdmin());
-		
-		assertTrue(adminSalvato.isPresent());
-		assertTrue(adminSalvato.get().getEmailAdmin().equals("mail@mail.it"));
-		
-	}
 
+    @Autowired 
+    private AmministratoriRepository ar;
+
+    @Test
+    void testFindByEmailAdmin() {
+        Amministratore admin = new Amministratore();
+        
+        admin.setNomeAdmin("Prova");
+        admin.setCognomeAdmin("Prova");
+        admin.setEmailAdmin("mail@mail.it");
+        admin.setPasswordAdmin("pass");
+        
+        ar.save(admin);
+        
+        Optional<Amministratore> adminSalvato = ar.findByEmailAdmin(admin.getEmailAdmin());
+        
+        assertTrue(adminSalvato.isPresent());
+        assertEquals("mail@mail.it", adminSalvato.get().getEmailAdmin());
+        
+        ar.deleteById(adminSalvato.get().getCodAdmin());
+    }
 }
