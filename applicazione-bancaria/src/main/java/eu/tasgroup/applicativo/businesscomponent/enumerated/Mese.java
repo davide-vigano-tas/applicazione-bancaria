877a2fc300807903b5495ac6aefec75925a50496
brusar <brusar@NBKMIPF4RBP6F.int.master.lan>
2024-12-12 17:05:01 +0100

    enum mese

---
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/businesscomponent/enumerated/Mese.java
@@ -0,0 +1,16 @@
+package eu.tasgroup.applicativo.businesscomponent.enumerated;
+
+public enum Mese {
+	GENNAIO,
+	FEBBRAIO,
+	MARZO,
+	APRILE,
+	MAGGIO,
+	GIUGNO,
+	LUGLIO,
+	AGOSTO,
+	SETTEMBRE,
+	OTTOBRE,
+	NOVEMBRE,
+	DICEMBRE
+}
