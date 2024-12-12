62046c594f91882ebf21192eb5fb2daf18d288c2
davide-vigano-tas <davide.vigano@tasgroup.eu>
2024-12-12 09:24:42 +0100

    fix package

--- applicazione-bancaria/src/test/java/eu/tasgroup/applicativo/repository/ClientiRepositoryTest.java
+++ applicazione-bancaria/src/test/java/eu/tasgroup/applicativo/repository/ClientiRepositoryTest.java
@@ -1,4 +1,4 @@
-package eu.tasgroup.applicativo.repository.mysql;
+package eu.tasgroup.applicativo.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
