1085a274cbc6b6a7324363ef85faf7caa645b870
davide-vigano-tas <davide.vigano@tasgroup.eu>
2024-12-12 16:11:07 +0100

    Add transazione bancaria service test

--- applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/businesscomponent/model/mysql/TransazioneBancaria.java
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/businesscomponent/model/mysql/TransazioneBancaria.java
@@ -2,6 +2,7 @@ package eu.tasgroup.applicativo.businesscomponent.model.mysql;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -43,4 +44,82 @@ public class TransazioneBancaria implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "cod_conto_destinazione", nullable = false)
 	private Conto contoDestinazione;
+
+	public long getCodTransazioneBancaria() {
+		return codTransazioneBancaria;
+	}
+
+	public void setCodTransazioneBancaria(long codTransazioneBancaria) {
+		this.codTransazioneBancaria = codTransazioneBancaria;
+	}
+
+	public double getImporto() {
+		return importo;
+	}
+
+	public void setImporto(double importo) {
+		this.importo = importo;
+	}
+
+	public Date getDataTransazione() {
+		return dataTransazione;
+	}
+
+	public void setDataTransazione(Date dataTransazione) {
+		this.dataTransazione = dataTransazione;
+	}
+
+	public TipoTransazione getTipoTransazione() {
+		return tipoTransazione;
+	}
+
+	public void setTipoTransazione(TipoTransazione tipoTransazione) {
+		this.tipoTransazione = tipoTransazione;
+	}
+
+	public Conto getContoOrigine() {
+		return contoOrigine;
+	}
+
+	public void setContoOrigine(Conto contoOrigine) {
+		this.contoOrigine = contoOrigine;
+	}
+
+	public Conto getContoDestinazione() {
+		return contoDestinazione;
+	}
+
+	public void setContoDestinazione(Conto contoDestinazione) {
+		this.contoDestinazione = contoDestinazione;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codTransazioneBancaria, contoDestinazione, contoOrigine, dataTransazione, importo,
+				tipoTransazione);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TransazioneBancaria other = (TransazioneBancaria) obj;
+		return codTransazioneBancaria == other.codTransazioneBancaria
+				&& Objects.equals(contoDestinazione, other.contoDestinazione)
+				&& Objects.equals(contoOrigine, other.contoOrigine)
+				&& Objects.equals(dataTransazione, other.dataTransazione)
+				&& Double.doubleToLongBits(importo) == Double.doubleToLongBits(other.importo)
+				&& tipoTransazione == other.tipoTransazione;
+	}
+
+	@Override
+	public String toString() {
+		return "TransazioneBancaria [codTransazioneBancaria=" + codTransazioneBancaria + ", importo=" + importo
+				+ ", dataTransazione=" + dataTransazione + ", tipoTransazione=" + tipoTransazione + ", contoOrigine="
+				+ contoOrigine + ", contoDestinazione=" + contoDestinazione + "]";
+	}
 }
---
+++ applicazione-bancaria/src/test/java/eu/tasgroup/applicativo/service/TransazioneBancariaServiceTest.java
@@ -0,0 +1,117 @@
+package eu.tasgroup.applicativo.service;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import eu.tasgroup.applicativo.businesscomponent.enumerated.TipoConto;
+import eu.tasgroup.applicativo.businesscomponent.enumerated.TipoTransazione;
+import eu.tasgroup.applicativo.businesscomponent.model.mysql.Cliente;
+import eu.tasgroup.applicativo.businesscomponent.model.mysql.Conto;
+import eu.tasgroup.applicativo.businesscomponent.model.mysql.TransazioneBancaria;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class TransazioneBancariaServiceTest {
+	
+	@Autowired
+	ClientiService clientiService;
+	@Autowired
+	ContiService contiService;
+	@Autowired
+	TransazioneBancariaService tbs;
+	
+	private static Cliente cliente; 
+	private static Conto contoOrigin;
+	private static Conto contoDest;
+	private static TransazioneBancaria transazioneBancaria;
+	
+	
+	@BeforeAll
+	static void setUpBeforeClass() {
+		cliente = new Cliente();
+		
+		cliente.setNomeCliente("Paolo");
+		cliente.setCognomeCliente("Rossi");
+		cliente.setEmailCliente("mail@mail.it");
+		cliente.setPasswordCliente("pass");
+		cliente.setSaldoConto(300);
+		
+		contoOrigin = new Conto();
+		contoOrigin.setSaldo(1000);
+		contoOrigin.setTipoConto(TipoConto.CORRENTE);
+		
+		contoDest = new Conto();
+		contoDest.setSaldo(1000);
+		contoDest.setTipoConto(TipoConto.RISPARMIO);
+		
+		transazioneBancaria = new TransazioneBancaria();
+		transazioneBancaria.setTipoTransazione(TipoTransazione.ACCREDITO);
+		transazioneBancaria.setImporto(100);
+		transazioneBancaria.setDataTransazione(new Date());
+	}
+	
+	@Test
+	@Order(1)
+	void testCreateOrUpdate() {
+		cliente = clientiService.createOrUpdate(cliente);
+		
+		contoOrigin.setCliente(cliente);
+		contoDest.setCliente(cliente);
+		
+		cliente.getConti().add(contoOrigin);
+		cliente.getConti().add(contoDest);
+		
+		System.out.println(cliente);
+		
+		cliente = clientiService.createOrUpdate(cliente);
+		
+		// recupero i conti 
+		contoOrigin = contiService.getAll().get(1);
+		contoDest = contiService.getAll().get(0);
+		
+		transazioneBancaria.setContoOrigine(contoOrigin);
+		transazioneBancaria.setContoDestinazione(contoDest);
+		transazioneBancaria = tbs.createOrUpdate(transazioneBancaria);
+	}
+
+	@Test
+	@Order(2)
+	void testGetTransazioneBancariaById() {
+		assertTrue(tbs.getTransazioneBancariaById(transazioneBancaria.getCodTransazioneBancaria()).isPresent());
+	}
+
+	@Test
+	@Order(3)
+	void testGetAll() {
+		assertFalse(tbs.getAll().isEmpty());
+	}
+	
+	@Test
+	@Order(4)
+	void testDeleteTransazioneBancariaById() {
+		tbs.deleteTransazioneBancariaById(transazioneBancaria.getCodTransazioneBancaria());
+		assertTrue(tbs.getAll().isEmpty());
+	}
+	
+	@AfterAll
+	static void tearDownAfterClass(@Autowired ContiService contiService , @Autowired ClientiService clientiService) {
+		for (Conto conto : contiService.getAll()) {
+			contiService.deleteContoById(conto.getCodConto());
+		}
+		
+		cliente = clientiService.findById(cliente.getCodCliente()).get();
+		clientiService.deleteCliente(cliente);
+	}
+
+}
