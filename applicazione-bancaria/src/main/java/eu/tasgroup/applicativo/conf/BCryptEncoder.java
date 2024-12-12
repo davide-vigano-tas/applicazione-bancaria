ed0f442437eec167ab8bf231d35fbdfc60fa1335
davide-vigano-tas <davide.vigano@tasgroup.eu>
2024-12-12 09:41:13 +0100

    ADD Encoder for Password

---
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/conf/BCryptEncoder.java
@@ -0,0 +1,23 @@
+package eu.tasgroup.applicativo.conf;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BCryptEncoder {
+
+    private static final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+    public static String encode(String pass) {
+        return bCryptPasswordEncoder.encode(pass);
+    }
+    
+    /**
+     * Confronta una password non criptata con una password criptata.
+     * 
+     * @param password password non criptata
+     * @param passwordDB password criptata, generalmente presa dal DB
+     * @return True se le password corrispondono, altrimenti false
+     */
+    public static boolean passwordEncodedMatch(String password, String passwordDB) {
+        return bCryptPasswordEncoder.matches(password, passwordDB);
+    }
+}
