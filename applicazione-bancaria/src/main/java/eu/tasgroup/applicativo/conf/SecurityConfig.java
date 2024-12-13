85827620c906770dd9cf467658baee65cba922db
davide-vigano-tas <davide.vigano@tasgroup.eu>
2024-12-13 11:48:30 +0100

    fix security filter 1

--- applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/conf/SecurityConfig.java
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/conf/SecurityConfig.java
@@ -24,15 +24,13 @@ public class SecurityConfig {
     public SecurityConfig(AmministratoriRepository ar, ClientiRepository cr) {
 		this.ar = ar;
 		this.cr = cr;
-	}
+	}                    
 
 	@Bean
 	@Order(1)
     SecurityFilterChain adminFilterChain(HttpSecurity http, CustomAuthenticationFailureHandler failureHandler) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
         )
         .formLogin((form) -> form
                         .loginPage("/admin-login")
@@ -51,39 +49,25 @@ public class SecurityConfig {
         return http.build();
     }
 	
-	@Bean
-	@Order(2)
-    SecurityFilterChain filterChain(HttpSecurity http, CustomAuthenticationFailureHandler failureHandler) throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers("/user/**").hasRole("USER")
-        )
-        .formLogin((form) -> form
-                        .loginPage("/user-login")
-                        .failureUrl("/user-login?error=true")
-                        .failureHandler(failureHandler)
-                        .defaultSuccessUrl("/user/", true)
-                        .permitAll()
-        )
-        .logout((logout) -> logout
-                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .permitAll()
-        );
-        return http.build();
-    }
-	
-	 @Bean
-	 @Order(3)
-	    SecurityFilterChain defaultFilterChain(HttpSecurity http,CustomAuthenticationFailureHandler failureHandler) throws Exception {
-	        http.authorizeHttpRequests(authorize -> authorize
-	            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-	            .anyRequest().permitAll()
-	            );
-	        return http.build();
-	    }
+	/*
+	 * @Bean
+	 * 
+	 * @Order(2) SecurityFilterChain clientefilterChain(HttpSecurity http) throws
+	 * Exception { http.authorizeHttpRequests((authorize) -> authorize
+	 * .requestMatchers("/user/**").hasRole("USER") ) .formLogin((form) -> form
+	 * .loginPage("/user-login") .failureUrl("/user-login?error=true")
+	 * .defaultSuccessUrl("/user/", true) .permitAll() ) .logout((logout) -> logout
+	 * .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+	 * .logoutSuccessUrl("/") .invalidateHttpSession(true)
+	 * .clearAuthentication(true) .permitAll() ); return http.build(); }
+	 * 
+	 * @Bean
+	 * 
+	 * @Order(3) SecurityFilterChain defaultFilterChain(HttpSecurity http) throws
+	 * Exception { http.authorizeHttpRequests(authorize -> authorize
+	 * .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).
+	 * permitAll() .anyRequest().permitAll() ); return http.build(); }
+	 */
 
     @Bean
     DaoAuthenticationProvider authenticationProvider() {
@@ -92,6 +76,7 @@ public class SecurityConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
+
 
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
