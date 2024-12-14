8192b86445bcec07895319c59caec74eef4e5042
samuel-mastrelli-tas <samuel.mastrelli@tasgroup.eu>
2024-12-14 20:10:34 +0100

    Added statisticheExtra route to AdminController

--- applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/controller/AdminController.java
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/controller/AdminController.java
@@ -16,14 +16,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import eu.tasgroup.applicativo.businesscomponent.enumerated.StatoRichiestaPrestito;
 import eu.tasgroup.applicativo.businesscomponent.enumerated.TipoTransazione;
 import eu.tasgroup.applicativo.businesscomponent.model.mongo.TransazioniMongo;
 import eu.tasgroup.applicativo.businesscomponent.model.mysql.Amministratore;
 import eu.tasgroup.applicativo.businesscomponent.model.mysql.Cliente;
+import eu.tasgroup.applicativo.businesscomponent.model.mysql.Prestito;
+import eu.tasgroup.applicativo.businesscomponent.model.mysql.Transazione;
 import eu.tasgroup.applicativo.service.AmministratoriService;
 import eu.tasgroup.applicativo.service.ClientiService;
 import eu.tasgroup.applicativo.service.ContiService;
 import eu.tasgroup.applicativo.service.PrestitoService;
+import eu.tasgroup.applicativo.service.RichiestePrestitoService;
 import eu.tasgroup.applicativo.service.TransazioneService;
 import eu.tasgroup.applicativo.service.TransazioniMongoService;
 
@@ -48,6 +52,9 @@ public class AdminController {
 	
 	@Autowired
 	PrestitoService prestitoService;
+	
+	@Autowired
+	RichiestePrestitoService richiestePrestitoService;
 	
 	@GetMapping({"", "/"})
 	public ModelAndView adminPage(@AuthenticationPrincipal UserDetails userDetails) {
@@ -147,6 +154,54 @@ public class AdminController {
 		return new ModelAndView("redirect:/admin/admin-login");
 	}
 	
+	@GetMapping("/statistiche_extra")
+	public ModelAndView statisticheExtra(@AuthenticationPrincipal UserDetails userDetails) {
+		ModelAndView mv = new ModelAndView("admin-statistiche-extra");
+		String email = userDetails.getUsername();
+		Optional<Amministratore> admin = amministratoriService.findByEmailAdmin(email);
+		if(admin.isPresent()) {
+			Amministratore ad = admin.get();
+			mv.addObject("admin", ad);
+			
+			long inAttesa = richiestePrestitoService.findByStatus(StatoRichiestaPrestito.IN_ATTESA).size();
+			long approvate = richiestePrestitoService.findByStatus(StatoRichiestaPrestito.APPROVATO).size();
+			long rifiutate = richiestePrestitoService.findByStatus(StatoRichiestaPrestito.RIFIUTATO).size();
+			
+			mv.addObject("admin_num_attesa", inAttesa);
+			mv.addObject("admin_num_approvate", approvate);
+			mv.addObject("admin_num_rifiutate", rifiutate);
+			
+			List<Prestito> approvati = prestitoService.getAll();
+			List<Cliente> clienti = clientiService.getClientiList();
+			Map<Cliente, Double> sommaImportoPresititi = new HashMap<Cliente, Double>();
+			
+			for(Cliente c : clienti) {
+				double somma = approvati.stream()
+						.filter((p) -> p.getCliente().getCodCliente() == c.getCodCliente())
+						.mapToDouble((p) -> p.getImporto()).sum();
+				sommaImportoPresititi.put(c, somma);
+			}
+			mv.addObject("admin_somma_prestiti_clienti", sommaImportoPresititi);
+			
+			int accrediti = transazioniMongoService.numeroTransazioniPerTipo(TipoTransazione.ACCREDITO);
+			int tot = transazioniMongoService.findAll().size();
+			
+			mv.addObject("admin_perc_accrediti", accrediti/tot);
+			Map<Cliente, List<TransazioniMongo>> tPerC = new HashMap<Cliente, List<TransazioniMongo>>();
+			Map<Cliente, Double> impMedioPerCliente = new HashMap<Cliente, Double>();
+			for(Cliente c : clienti) {
+				tPerC.put(c, transazioniMongoService.findByCliente(c.getCodCliente()));
+				impMedioPerCliente.put(c, 
+						transazioniMongoService.calcoloMediaTransazioniPerCliente(c.getCodCliente()));
+			}
+			
+			mv.addObject("admin_t_per_cliente", tPerC);
+			mv.addObject("admin_importo_medio_cliente", impMedioPerCliente);
+			
+			return mv;
+		} 
+		return new ModelAndView("redirect:/admin/admin-login");
+	}
 	
 
 }
