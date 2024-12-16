a2e4540cc76c3fcccb754dbf14b25674bcb390d3
samuel-mastrelli-tas <samuel.mastrelli@tasgroup.eu>
2024-12-16 09:46:22 +0100

    Admincontroller: Conti a zero, dettagli cliente, add cliente

--- applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/controller/AdminController.java
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/controller/AdminController.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,9 +22,12 @@ import eu.tasgroup.applicativo.businesscomponent.enumerated.StatoRichiestaPresti
 import eu.tasgroup.applicativo.businesscomponent.enumerated.TipoTransazione;
 import eu.tasgroup.applicativo.businesscomponent.model.mongo.TransazioniMongo;
 import eu.tasgroup.applicativo.businesscomponent.model.mysql.Amministratore;
+import eu.tasgroup.applicativo.businesscomponent.model.mysql.Carta;
 import eu.tasgroup.applicativo.businesscomponent.model.mysql.Cliente;
+import eu.tasgroup.applicativo.businesscomponent.model.mysql.Conto;
 import eu.tasgroup.applicativo.businesscomponent.model.mysql.Prestito;
 import eu.tasgroup.applicativo.businesscomponent.model.mysql.Transazione;
+import eu.tasgroup.applicativo.repository.TransazioneMongoRepository;
 import eu.tasgroup.applicativo.service.AmministratoriService;
 import eu.tasgroup.applicativo.service.ClientiService;
 import eu.tasgroup.applicativo.service.ContiService;
@@ -71,6 +76,15 @@ public class AdminController {
 			
 		} 
 		return new ModelAndView("redirect:/admin/admin-login");
+		
+	}
+	@PostMapping("/nuovoCliente")
+	public ModelAndView nuovoClienteForm(Cliente cliente) {
+	
+
+			clientiService.createOrUpdate(cliente);
+			
+			return new ModelAndView("redirect:/admin/");
 		
 	}
 	
@@ -203,5 +217,60 @@ public class AdminController {
 		return new ModelAndView("redirect:/admin/admin-login");
 	}
 	
+	@GetMapping("/contizero")
+	public ModelAndView contiZeroSaldo(@AuthenticationPrincipal UserDetails userDetails) {
+		ModelAndView mv = new ModelAndView("admin-contizero");
+		String email = userDetails.getUsername();
+		Optional<Amministratore> admin = amministratoriService.findByEmailAdmin(email);
+		if(admin.isPresent()) {
+			Amministratore ad = admin.get();
+			mv.addObject("admin", ad);
+			List<Conto> contiZero = contiService.getContiSaldoZero();
+			mv.addObject("admin_contizero", contiZero);
+			return mv;
+			
+		} 
+		return new ModelAndView("redirect:/admin/admin-login");
+	}
+	
+	@GetMapping("/eliminaConto/{id}")
+	public ModelAndView eliminaConto(@PathVariable long id) {
+		if(contiService.findById(id).isPresent()) {
+			contiService.deleteContoById(id);
+			return new ModelAndView("redirect:/admin/contizero");
+		}
+		return new ModelAndView("redirect:/admin/contizero");
+	}
+	
 
+	@GetMapping("/riepilogoCliente/{id}") 
+	public ModelAndView riepilogo(@PathVariable long id, @AuthenticationPrincipal UserDetails userDetails) {
+		ModelAndView mv = new ModelAndView("admin-cliente-riepilogo");
+		String email = userDetails.getUsername();
+		Optional<Amministratore> admin = amministratoriService.findByEmailAdmin(email);
+		if(admin.isPresent()) {
+			Amministratore ad = admin.get();
+			mv.addObject("admin", ad);
+			if(clientiService.findById(id).isPresent()) {
+				Cliente cliente = clientiService.findById(id).get();
+				mv.addObject("admin_cliente", cliente);
+				List<Conto> contiCliente = new ArrayList<Conto>(
+						cliente.getConti());
+				List<Carta> carteCliente = new ArrayList<Carta>(
+						cliente.getCarte());
+				List<Prestito> prestitiCliente = clientiService.listaPrestitiClienti(cliente.getCodCliente());
+				List<TransazioniMongo> transazioniCliente = transazioniMongoService.findByCliente(cliente.getCodCliente());
+				
+				mv.addObject("admin_conti_cliente", contiCliente);
+				mv.addObject("admin_carte_cliente", carteCliente);
+				mv.addObject("admin_prestiti_cliente", prestitiCliente);
+				mv.addObject("admin_transazioni_cliente", transazioniCliente);
+
+				return mv;
+			}
+			return new ModelAndView("redirect:/admin/");
+			
+		} 
+		return new ModelAndView("redirect:/admin/admin-login");
+	}
 }
