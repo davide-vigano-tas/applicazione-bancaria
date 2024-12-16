1d4536adc201cdb9ce6584282908ce9573a9ba10
samuel-mastrelli-tas <samuel.mastrelli@tasgroup.eu>
2024-12-16 10:14:57 +0100

    Added admin-page

--- applicazione-bancaria/SQL/dati.sql
+++ applicazione-bancaria/SQL/dati.sql
@@ -1,4 +1,4 @@
--- ADMIN 
+-- ADMIN --Pass01$
 
 INSERT INTO amministratori(cod_admin,nome_admin,cognome_admin,email_admin,password_admin,tentativi_errati) 
 VALUES (1,"Mario", "Rossi", "mario.rossi@tasgroup.eu", "$2a$10$YhROqGpOSPT.BBesLIPwq.p72Bxh.UgbIK9tYzp.Hh47x0hKWeVp6",0);
