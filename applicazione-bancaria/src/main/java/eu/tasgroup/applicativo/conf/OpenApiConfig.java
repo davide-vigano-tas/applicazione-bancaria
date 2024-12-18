430524fad830d0518b8d2e2ad3351d2fab679614
Roberto-B-tas <roberto.brusa@tasgroup.eu>
2024-12-18 10:42:35 +0100

    swagger

---
+++ applicazione-bancaria/src/main/java/eu/tasgroup/applicativo/conf/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package eu.tasgroup.applicativo.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+	@Bean
+	public OpenAPI CustomOpenAPI() {
+		SecurityScheme bearerScheme = new SecurityScheme()
+				.type(SecurityScheme.Type.HTTP)
+				.scheme("bearer")
+				.bearerFormat("JWT");
+		
+		return new OpenAPI()
+				.components(new Components().addSecuritySchemes("bearerAuth", bearerScheme))
+				.addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+	}
+}
