8e860b8073754bdf770a68a87e4dc3b955d80057
davide-vigano-tas <davide.vigano@tasgroup.eu>
2024-12-18 13:03:36 +0100

    add auth cliente conto service

---
+++ frontend/frontend-applicazionebancaria/src/app/Service/auth-service.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private baseUrl: string = 'http://localhost:8080/api';
+  private tokenKey: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, { email, password });
+  }
+
+  setToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.tokenKey);
+  }
+}
